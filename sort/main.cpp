36769e1df66746762ea4bb9562caafb8d758d063
Kyan Mulji <muljikyan@gmail.com>
2023-11-24 04:29:30 -0500

    sort bubble

--- sort/main.cpp
+++ sort/main.cpp
@@ -3,8 +3,23 @@
 
 using std::vector;
 void BubbleSort(vector<int> &nums){
-    
+    for(int i=0;i<nums.size();i++){
+        for(int j=0;j<nums.size()-i-1;j++){
+            if(nums[j]>nums[j+1]){
+                int temp = nums[j];
+                nums[j]=nums[j+1];
+                nums[j+1]=temp;
+            }
+        }
+    }
 }
 int main()
 {
+    vector<int> n{1,2,5,9,7};
+    BubbleSort(n);
+
+    for(int i=0;i<n.size();i++){
+        std::cout<<n[i]<<std::endl;
+    }
+    return 0;
 }
