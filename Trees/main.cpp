d5b0aed17513b040e6aa99462e8e22eedbd5830c
Kyan Mulji <muljikyan@gmail.com>
2024-01-15 06:48:38 -0500

    done

---
+++ Trees/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+/*Tree Traversals
+    1. pre order
+    2. in order
+    3. post order
+    
+    For Inorder, you traverse from the left subtree to the root then to the right subtree.
+    For Preorder, you traverse from the root to the left subtree then to the right subtree.
+    For Post order, you traverse from the left subtree to the right subtree then to the root.
+
+*/
+
+int main()
+{
+}
